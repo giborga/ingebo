1cef7bf6fd768475d6bf20a3c43346947cb9eadb
giborga <giborga.palionite@gmail.com>
2022-03-20 23:53:59 +0400

    CRUD for users

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+# main.py
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# endpoint /users
+@app.get("/users")
+async def read_users():
+    return {"users": ['user1', 'user2', 'user3', 'user4', 'user5']}
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+@app.get("/users/{id}")
+async def read_user(id: int):
+    return {"user": "User 1"}
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class User(BaseModel):
+    name: str
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@app.post("/users")
+async def create_user(user: User):
+    return {"status": 'User {} created'.format(user.name)}
+
+
+@app.put("/users/{id}")
+async def update_user(id: int, user: User):
+    return {"status": 'User with id {}, {} was updated'.format(id, user.name)}
+
+
+@app.delete("/users/{id}")
+async def delete_user(id: int):
+    return {"status": 'User {} deleted'.format(id)}
